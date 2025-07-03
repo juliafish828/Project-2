cf20d0232853e9ff46e01445ef0e213de659c1b2
Julia Fish <juliafish28@gmail.com>
2025-07-03 19:39:00 -0400

    about tab almost done, theme added

--- app.R
+++ app.R
@@ -1,29 +1,74 @@
+library(bslib)
 source("helpers.R")
 
 ui <- fluidPage(
+  
+  theme = bs_theme(
+    version = 5,
+    bootswatch = "minty",
+    base_font = font_google("Noto Sans") # Specify your custom font here
+  ),
+  
   titlePanel("Brewery Information for Virignians, North Carolinians, and Visitors :)"),
+  
+  tags$br(),
+  
   sidebarLayout(
     sidebarPanel(
-      selectInput("state", "Choose a state", choices = c("North Carolina", "Virginia"))
+      conditionalPanel(
+        condition = "input.main_tabs == 'About'",
+        h5("Purpose/Guide of Each Tab:"),
+        
+        tags$br(),
+        
+        strong("About Tab:"),
+        p("Discusses the overall purpose of the app as well as the data source information."),
+        
+        tags$br(),
+        
+        strong("Data Download Tab:"),
+        p("Querying the API for the data. Essentially, you will be choosing the data in
+          which you want to look more into using this app."),
+        
+        tags$br(),
+        
+        strong("Data Exploration Tab:"),
+        p("The place in which you will look at the data you selected in the data download tab.
+          You can look at numerical summaries as well as build interactive plots to visualize
+          and understand the chosen data.")
+        
+      ),
+      
+      conditionalPanel(
+        condition = "input.main_tabs == 'Data Download'",
+        selectInput("state", "Choose a state", choices = c("North Carolina", "Virginia"))
+      ), # close conditional panel
     ), # close sidebar panel
+    
     mainPanel(
       tabsetPanel(
         id = "main_tabs",
         tabPanel(
           "About",
-          strong(h2("The Purpose of the Site:")),
-          p("This site is here for you to be able to gather information on breweries in the area.
+          
+          tags$br(),
+          
+          strong(h2("Purpose of the App:")),
+          p("This app is here for you to be able to gather information on breweries in the area.
             Between the states of North Carolina and Virginia, there is plenty of growth with
-            population and business opportunity. Acknowledging this, we designed this website with
-            you in mind: the visitors and newcomers."),
+            population and business opportunity. Acknowledging this, this app was designed with
+            you in mind: the visitors and newcomers to these two lovely states!"),
+          
           p("As a very recent newcomer from Virginia to North Carolina, I remember how it felt to
-            not know about the area as well as not know how to get started in fixing that. Here is
-            your solution!"),
+            not only know nothing about the are, but also not know how to get started in fixing that.
+            Here is (hopefully) part of your solution!"),
+          
           h2("Data Information and its Source:"),
           p("This data has been collected from the "),
-          h2("Purpose of Each Tab:"),
-          p("ourpose"),
-          h2("i need a pic here"),
+          
+          column(width = 12, align = "center",
+                 img(src = "beer_buddies.png", height = "400px")
+          )
         ),
         tabPanel(
           "Data Download",
@@ -40,6 +85,7 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
+  bs_themer()
   data_input <- reactive({
     get_breweries(input$state, input$city, input$type)
   })
