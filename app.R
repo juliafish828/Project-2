e050f72fbb648d1e07a2d84f740b208a55f3a8b8
Julia Fish <juliafish28@gmail.com>
2025-07-04 13:08:59 -0400

    data download complete, comments added to help later

--- app.R
+++ app.R
@@ -6,7 +6,7 @@ ui <- fluidPage(
   theme = bs_theme(
     version = 5,
     bootswatch = "minty",
-    base_font = font_google("Noto Sans") # Specify your custom font here
+    base_font = font_google("Noto Sans")
   ),
   
   tags$br(),
@@ -51,6 +51,7 @@ ui <- fluidPage(
         tags$br(),
         tags$br(),
         
+        # dynamic options for city based on state selection with helper text to pop up if needed
         uiOutput("city_ui"),
         uiOutput("helper_text"),
         
@@ -58,12 +59,13 @@ ui <- fluidPage(
         tags$br(),
         tags$br(),
         
+        # dynamic brewery type options
         uiOutput("type_ui"),
         
-        # Column selector
+        # specifying the desired columns for table and .csv
         uiOutput("column_ui"),
         
-        # Row filter (number of rows to keep)
+        # specifying the desired rows to keep for table and .csv
         numericInput("n_rows", "Number of rows to show:", value = 10, min = 1),
         
         downloadButton("download_data", "Download CSV")
@@ -102,6 +104,7 @@ ui <- fluidPage(
             all types of breweries to be brought down from the site, though!"
           ),
           
+          # center and put the image on app
           column(width = 12, align = "center",
                  img(src = "beer_buddies.png", height = "400px")
           )
@@ -111,10 +114,13 @@ ui <- fluidPage(
           
           tags$br(),
           
+          # data table
           tableOutput("filtered_table")
         ),
         tabPanel(
           "Data Exploration",
+          
+          # dynamic plot
           plotOutput("brew_plot")
         )
       )
@@ -124,19 +130,52 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-
-  # 1. Get filtered brewery data from API
-  filtered_brewery_data <- reactive({
-    get_breweries(
-      state = if (input$state_input != "Both/No Selection") input$state_input else NULL,
-      city  = if (!is.null(input$city_input) && input$city_input != "") input$city_input else NULL
-    )
+  
+  # reactive data source to let user query the API
+  data_input <- reactive({
+    req(input$choose_state)
+    
+    cities <- input$choose_city
+    types <- input$choose_type
+    
+    # If no state selected ("Both/No Selection"), return all data with only potential filter for type
+    if (input$choose_state == "Both/No Selection") {
+      all_data <- get_breweries()
+      
+      if (!is.null(input$choose_type) && length(input$choose_type) > 0) {
+        all_data <- filter(all_data, brewery_type %in% input$choose_type)
+      }
+      
+      return(all_data)
+    }
+    
+    # if no city selection, all cities should be selected (the values depending on which state)
+    if (is.null(cities) || length(cities) == 0) {
+      cities <- if (input$choose_state == "North Carolina") city_options_nc else city_options_va
+    }
+    
+    # if there is more than 1 city selected, get the breweries for each city and bind them
+    if (length(cities) > 1) {
+      all_data <- map(cities, function(city) {
+        get_breweries(state = input$choose_state, city = city)
+      }) |> bind_rows()
+    } else {
+      all_data <- get_breweries(state = input$choose_state, city = cities) # if one city, get those
+    }
+    
+    # if there are types selected, filter the data by those types
+    if (!is.null(types) && length(types) > 0) {
+      all_data <- filter(all_data, brewery_type %in% types)
+    }
+    
+    all_data
   })
   
-  # Building the select input with the appropriate cities inside given the selected state
+  # putting the appropriate cities ONLY into the checkbox group input
   output$city_ui <- renderUI({
-    req(input$choose_state != "Both/No Selection")
+    req(input$choose_state != "Both/No Selection") # if no state selected, no city can be selected
     
+    # checkbox being built for each state selection
     if (input$choose_state == "North Carolina"){
 
       checkboxGroupInput("choose_city", "Select all desired NC cities:",
@@ -151,6 +190,7 @@ server <- function(input, output, session) {
     
   })
   
+  # text explaining that not selecting any state is the same as selecting all available
   output$helper_text <- renderUI({
     req(input$choose_state != "Both/No Selection")
     
@@ -159,36 +199,47 @@ server <- function(input, output, session) {
              all cities.)")
   })
   
+  # dynamically change the options available to select for type based on state and city
   output$type_ui <- renderUI({
     
-    # if state and city are both NULL
+    # if "Both/No Selection" is chosen for state, all brewery types are shown
     type_choices <- get_breweries()$brewery_type |>
       unique() |>
       sort()
     
-    
+    # if one of the states is chosen...
     if (input$choose_state == "North Carolina" || input$choose_state == "Virginia"){
       
+      # and all (or none) of the cities are selected for that state...
       if (is.null(input$choose_city) || setequal(input$choose_city, city_options_nc) ||
           setequal(input$choose_city,city_options_va)){ # if none or all in NC or VA
         
+        # the type options are those available for the selected state
         type_choices <- get_breweries(state = input$choose_state)$brewery_type |>
           unique() |>
           sort()
         
+        # if the state is Virginia (3 options here) and at least one city is selected...
       } else if (input$choose_state == "Virginia" && !is.null(input$choose_city)){
+        
+        # if one city is selected...
         if (length(input$choose_city) == 1){
           
+          # the type options are those in that one city
           type_choices <- get_breweries(state = input$choose_state,
                                         city = input$choose_city)$brewery_type |>
             unique() |>
             sort()
+          
+          # if 2 Virginia cities are selected...
         } else if (length(input$choose_city) == 2){
           
+          # we go one by one through the cities and get separate dfs with all breweries for wach city
           all_data <- map(input$choose_city, function(city) {
             get_breweries(state = input$choose_state, city = city)
-          }) |> bind_rows()
+          }) |> bind_rows() # then put them together to have all the breweries in those cities together
           
+          # then the type options are the sorted, unique brewery types from this data frame
           type_choices <- all_data$brewery_type |>
             unique() |>
             sort()
@@ -199,17 +250,49 @@ server <- function(input, output, session) {
     
     }
 
-    
+    # finally, the checkbox is made with the appropriate type options in it
     checkboxGroupInput("choose_type", "Choose brewery type(s):",
                        choices = type_choices,
                        selected = type_choices)
     
   })
   
+  # the data table is made with the selected amount of rows and columns
+  output$filtered_table <- renderTable({
+    req(input$selected_columns)
+    head(data_input()[, input$selected_columns, drop = FALSE], input$n_rows)
+  })
+  
+  # the plot is made (very simple for now)
   output$brew_plot <- renderPlot({
     plot_breweries(data_input())
   })
+  
+  # the columns checkbox for the data table
+  output$column_ui <- renderUI({
+    req(data_input())
+    
+    checkboxGroupInput(
+      "selected_columns",
+      "Select columns to display:",
+      choices = names(data_input()),
+      selected = names(data_input())
+    )
+  })
+  
+  # the ability to download the selected data as a .csv file
+  output$download_data <- downloadHandler(
+    filename = function() {
+      paste0("brewery_data_", Sys.Date(), ".csv")
+    },
+    content = function(file) {
+      req(input$selected_columns)
+      write.csv(data_input()[, input$selected_columns, drop = FALSE], file, row.names = FALSE)
+    }
+  )
+  
 }
 
+# run the app
 shinyApp(ui, server)
 
--- helpers.R
+++ helpers.R
@@ -3,27 +3,32 @@ library(jsonlite)
 library(tibble)
 library(dplyr)
 
+# to use in the app
 city_options_nc <- c("Durham", "Raleigh")
 city_options_va <- c("Fredericksburg", "Richmond", "Virginia Beach")
 
 
+# function used to query the API
 get_breweries <- function(state = NULL, city = NULL, type = NULL, per_page = 50, page = 1) {
   
   base_url <- "https://api.openbrewerydb.org/v1/breweries"
   
-  # Initialize all query parameters
+  # start blank (GPP)
   by_state <- NULL
   by_city <- NULL
   brewery_type <- NULL
   
+  # takes the string, makes spaces underscores and all the letters lowercase (to use in the API)
   if (!is.null(state)) {
     by_state <- tolower(gsub(" ", "_", state))
   }
   
+  # same for cities
   if (!is.null(city)){
     by_city <- tolower(gsub(" ", "_", city))
   }
   
+  # make the query list outside of GET()
   query <- list(
     by_state = by_state,
     by_city = by_city,
@@ -32,35 +37,23 @@ get_breweries <- function(state = NULL, city = NULL, type = NULL, per_page = 50,
     page = page
   )
   
-  # Remove NULLs from query list
+  # remove NULLS (so they are not in the API and will not cause issues)
   query <- query[!sapply(query, is.null)]
   
-  # Make GET request
+  # GET(), parse, and return this tibble
   response <- GET(url = base_url, query = query)
-  
-  if (status_code(response) != 200) {
-    stop("API request failed with status code: ", status_code(response))
-  }
-  
   parsed <- fromJSON(rawToChar(response$content))
   
-  #data <- content(response, as = "text", encoding = "UTF-8")
-  #parsed <- fromJSON(data, flatten = TRUE)
   
   return(as_tibble(parsed))
 }
 
-# wrapper function for querying the API
-
-get_data <- function(state, city, type) {
-  get_breweries(state = state, city = city, type = type)
-}
-
-
+# to help with plotting on the app
 plot_breweries <- function(data) {
   ggplot(data, aes(x = brewery_type)) + geom_bar()
 }
 
+# potential to use for later
 summarize_breweries <- function(data) {
   data %>%
     group_by(brewery_type) %>%
